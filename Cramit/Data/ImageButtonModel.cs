92d457fb1091a1e94301130677dd1dd8ed2355c5
Tormod Fjeldskår <tormod.fjeldskaar@gmail.com>
2012-08-29 23:31:08 +0200

    Much of manifest as well as UI for main page completed.

---
+++ Cramit/Data/ImageButtonModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Media;
+
+namespace Cramit.Data
+{
+    public class ImageButtonModel
+    {
+        public int ID { get; set; }
+
+        public ImageSource Image { get; set; }
+
+        public string Text { get; set; }
+    }
+}
---
+++ Cramit/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using Cramit.Data;
+using System;
+using System.Collections.Generic;
+using Windows.UI.Xaml.Controls;
+
+// The Items Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234233
+
+namespace Cramit
+{
+    /// <summary>
+    /// A page that displays a collection of item previews.  In the Split Application this page
+    /// is used to display and select one of the available groups.
+    /// </summary>
+    public sealed partial class MainPage : Cramit.Common.LayoutAwarePage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Populates the page with content passed during navigation.  Any saved state is also
+        /// provided when recreating a page from a prior session.
+        /// </summary>
+        /// <param name="navigationParameter">The parameter value passed to
+        /// <see cref="Frame.Navigate(Type, Object)"/> when this page was initially requested.
+        /// </param>
+        /// <param name="pageState">A dictionary of state preserved by this page during an earlier
+        /// session.  This will be null the first time a page is visited.</param>
+        protected override void LoadState(Object navigationParameter, Dictionary<String, Object> pageState)
+        {
+            // TODO: Create an appropriate data model for your problem domain to replace the sample data
+            //var sampleDataGroups = SampleDataSource.GetGroups((String)navigationParameter);
+            //this.DefaultViewModel["Items"] = sampleDataGroups;
+        }
+
+        /// <summary>
+        /// Invoked when an item is clicked.
+        /// </summary>
+        /// <param name="sender">The GridView (or ListView when the application is snapped)
+        /// displaying the item clicked.</param>
+        /// <param name="e">Event data that describes the item clicked.</param>
+        void ItemView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            var buttonId = ((ImageButtonModel)e.ClickedItem).ID;
+            switch (buttonId)
+            {
+                case 1: // Quiz definitions
+                    // TODO
+                    // this.Frame.Navigate(typeof(QuizDefinitionsPage));
+                    break;
+
+                case 2: // Take quiz
+                    // TODO
+                    // this.Frame.Navigate(typeof(TakeQuizPage));
+                    break;
+            }
+        }
+    }
+}
